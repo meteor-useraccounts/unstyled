331e07ade118e7f226d01376fc9c90c18144fbde
Luca Mussi <ing.luca.mussi@gmail.com>
2015-02-15 21:16:22 +0100

    tentative new structure

--- package.js
+++ package.js
@@ -1,77 +1,54 @@
+// Package metadata for Meteor.js web platform (https://www.meteor.com/)
+// This file is defined within the Meteor documentation at
+//
+//   http://docs.meteor.com/#/full/packagejs
+//
+// and it is needed to define a Meteor package
+
+var Both = ['client', 'server'];
+var Client = 'client';
+var Server = 'server';
+
+
 Package.describe({
-    summary: "Accounts Templates unstyled.",
-    version: "1.7.0",
-    name: "useraccounts:unstyled",
-    git: "https://github.com/meteor-useraccounts/unstyled.git",
+	summary: "Accounts Templates unstyled.",
+	version: "2.0.0",
+	name: "useraccounts:unstyled",
+	git: "https://github.com/meteor-useraccounts/unstyled.git",
 });
 
 Package.on_use(function(api, where) {
-    api.versionsFrom("METEOR@1.0");
+	api.versionsFrom("METEOR@1.0");
 
-    api.use([
-        "less",
-        "templating",
-    ], "client");
+	api.use([
+		"templating",
+	], Client);
 
-    api.use([
-        "useraccounts:core",
-    ], ["client", "server"]);
+	api.use([
+		"useraccounts:init@2.0.0",
+	], Both);
 
-    api.imply([
-        "useraccounts:core@1.7.0",
-    ], ["client", "server"]);
+	api.imply([
+		"useraccounts:init",
+	], Both);
 
-    api.add_files([
-        "lib/at_error.html",
-        "lib/at_error.js",
-        "lib/at_form.html",
-        "lib/at_form.js",
-        "lib/at_input.html",
-        "lib/at_input.js",
-        "lib/at_nav_button.html",
-        "lib/at_nav_button.js",
-        "lib/at_oauth.html",
-        "lib/at_oauth.js",
-        "lib/at_pwd_form.html",
-        "lib/at_pwd_form.js",
-        "lib/at_pwd_form_btn.html",
-        "lib/at_pwd_form_btn.js",
-        "lib/at_pwd_link.html",
-        "lib/at_pwd_link.js",
-        "lib/at_reCaptcha.html",
-        "lib/at_reCaptcha.js",
-        "lib/at_result.html",
-        "lib/at_result.js",
-        "lib/at_sep.html",
-        "lib/at_sep.js",
-        "lib/at_signin_link.html",
-        "lib/at_signin_link.js",
-        "lib/at_signup_link.html",
-        "lib/at_signup_link.js",
-        "lib/at_social.html",
-        "lib/at_social.js",
-        "lib/at_terms_link.html",
-        "lib/at_terms_link.js",
-        "lib/at_title.html",
-        "lib/at_title.js",
-        "lib/full_page_at_form.html",
-        "lib/at_unstyled.css"
-    ], ["client"]);
-});
-
-Package.on_test(function(api) {
-    api.use([
-        "useraccounts:unstyled",
-        "useraccounts:core@1.7.0",
-    ]);
-
-    api.use([
-        "accounts-password",
-        "tinytest",
-        "test-helpers"
-    ], ["client", "server"]);
-
-    api.add_files([
-        "tests/tests.js"
-    ], ["client", "server"]);
+	api.add_files([
+		"src/full_page_ua_form.html",
+		"src/ua.css",
+		"src/ua_error.html",
+		"src/ua_input.html",
+		"src/ua_nav_button.html",
+		"src/ua_oauth.html",
+		"src/ua_pwd_form_btn.html",
+		"src/ua_pwd_form.html",
+		"src/ua_pwd_link.html",
+		"src/ua_reCaptcha.html",
+		"src/ua_result.html",
+		"src/ua_sep.html",
+		"src/ua_signin_link.html",
+		"src/ua_signup_link.html",
+		"src/ua_social.html",
+		"src/ua_terms_link.html",
+		"src/ua_title.html",
+	], Client);
 });
